fbd6b6dd4c87b83b78e717dab33fb1acdf96b225
Henrik <h.h.olsson@gmail.com>
2013-01-21 10:46:21 +0100

    adding sourcefiles

---
+++ VC++/lol/ArtigFranz/ArtigFranz/ArtigFranz.cpp
@@ -0,0 +1,15 @@
+// ArtigFranz.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	cout<<"lols"<<endl;
+	return 0;
+}
+
